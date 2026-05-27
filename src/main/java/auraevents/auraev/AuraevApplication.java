e092a1222770deb2aaca79949a89a85ff295f613
Samuel Lorand <samuel.lorand@hotmail.com>
2026-05-27 16:07:34 -0300

    Implementa estrutura inicial do Backend

---
+++ src/main/java/auraevents/auraev/AuraevApplication.java
@@ -0,0 +1,13 @@
+package auraevents.auraev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuraevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/auraevents/auraev/AuraevApplicationTests.java
@@ -0,0 +1,13 @@
+package auraevents.auraev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
